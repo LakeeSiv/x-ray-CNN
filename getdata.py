e6a5dd08cd7ce800bdfd7ca3956a9a7d805b2396
Lakee Sivaraya <lakeesivaraya@gmail.com>
2020-12-23 13:39:42 +0000

    Cleaned up getdata.py and stored the training data

--- getdata.py
+++ getdata.py
@@ -1,39 +1,67 @@
 import os
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3' 
-from tensorflow import keras
 import numpy as np
-
 from matplotlib.image import imread
 from glob import glob
+import random
+import cv2
+import matplotlib.pyplot as plt
 
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
 
-TEST_LOC = "./chest_xray/test"
-TRAIN_LOC = "./chest_xray/train"
+TEST_LOC = "./chest_xray/test/"
+TRAIN_LOC = "./chest_xray/train/"
 
 """
 N --> Normal
 P --> Pneumonia
 """
+CATEGORIES = ["NORMAL","PNEUMONIA"]
 
-TRAIN_LOC_N = TRAIN_LOC + "/NORMAL/"
-TRAIN_LOC_P = TRAIN_LOC + "/PNEUMONIA/"
-
-IMG_P = glob(TRAIN_LOC_P + "*.jpeg")
-IMG_N = glob(TRAIN_LOC_N + "*.jpeg")
-
-
-import random
-import cv2
-import matplotlib.pyplot as plt
-BATCH_SIZE = 32
 IMG_SIZE = 64
-SAMPLE_SIZE = 20
+
 
 train_set = []
 
+#get train data
+def make_traindata(condition = False):
+    if condition == True:
+        for category in CATEGORIES:
+            path = TRAIN_LOC + category +"/"
+            label = CATEGORIES.index(category)
+            for img in glob(path + "*.jpeg"):
+                image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
+                image_sized = cv2.resize(image,(IMG_SIZE,IMG_SIZE))
+
+                train_set.append([image_sized,label])
+
+        random.shuffle(train_set)
+
+        X,y=[],[]
+
+        for features,label in train_set:
+            X.append(features)
+            y.append(label)
+
+        X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
+
+        np.save("X_train.npy",X)
+        np.save("y_train.npy",y)
+
+
+
+
+make_traindata(condition = False)
+
+
+
+
+
+
+
+
+
+
+
+"""
 
 for im in random.sample(IMG_P,int(SAMPLE_SIZE/2)):
     
@@ -63,3 +91,4 @@ for features,label in train_set:
 X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 
 print(y)
+"""
