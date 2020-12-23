c0b8eebcfd4abe8f74df892d7fe6ab17b513de86
Lakee Sivaraya <lakeesivaraya@gmail.com>
2020-12-23 14:15:19 +0000

    Made test data files, update README

--- getdata.py
+++ getdata.py
@@ -22,16 +22,17 @@ IMG_SIZE = 64
 train_set = []
 
 #get train data
-def make_traindata(condition = False):
+def make_traindata(condition = False, LOC=0):
     if condition == True:
         for category in CATEGORIES:
-            path = TRAIN_LOC + category +"/"
+            path = LOC + category +"/"
             label = CATEGORIES.index(category)
             for img in glob(path + "*.jpeg"):
                 image = cv2.imread(img, cv2.IMREAD_GRAYSCALE)
                 image_sized = cv2.resize(image,(IMG_SIZE,IMG_SIZE))
 
                 train_set.append([image_sized,label])
+                
 
         random.shuffle(train_set)
 
@@ -43,52 +44,15 @@ def make_traindata(condition = False):
 
         X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
 
-        np.save("X_train.npy",X)
-        np.save("y_train.npy",y)
+
+        if LOC == TRAIN_LOC:
+            sub = "train"
+        elif LOC == TEST_LOC:
+            sub = "test"
+
+        np.save(f"X_{sub}.npy",X)
+        np.save(f"y_{sub}.npy",y)
 
 
+make_traindata(condition = False, LOC = TEST_LOC)
 
-
-make_traindata(condition = False)
-
-
-
-
-
-
-
-
-
-
-
-"""
-
-for im in random.sample(IMG_P,int(SAMPLE_SIZE/2)):
-    
-    image = cv2.imread(im, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (IMG_SIZE,IMG_SIZE))
-    
-    train_set.append([image,1])
-   
-
-for im in random.sample(IMG_N,int(SAMPLE_SIZE/2)):
-  
-    image = cv2.imread(im, cv2.IMREAD_GRAYSCALE)
-    image = cv2.resize(image, (IMG_SIZE,IMG_SIZE))
-    
-    train_set.append([image,0])
-   
-
-random.shuffle(train_set)
-
-
-X,y=[],[]
-
-for features,label in train_set:
-    X.append(features)
-    y.append(label)
-
-X = np.array(X).reshape(-1,IMG_SIZE,IMG_SIZE,1)
-
-print(y)
-"""
