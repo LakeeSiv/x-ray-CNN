df75ad5d224d6664ae8b65e9abedd16915f3f279
Lakee Sivaraya <lakeesivaraya@gmail.com>
2020-12-25 00:26:43 +0000

    implemented keras tuner

--- model.py
+++ model.py
@@ -4,6 +4,7 @@ os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 from tensorflow import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout
+from kerastuner.tuners import RandomSearch
 
 EPOCHS = 5
 
@@ -19,28 +20,46 @@ X_train = X_train/255
 X_test = X_test/255
 
 
-#<----------------Model----------------->
-model = Sequential()
+def build_model(hp):
+    model = Sequential()
 
-model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1), padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Conv2D(32, (3, 3), activation='relu', input_shape=(64, 64, 1), padding='same'))
+    model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Conv2D(32, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
+    for i in range(hp.Int('ConvLayers', 2, 3)):
+        model.add(Conv2D(hp.Choice("filters", values = [32,64]), (3, 3), activation='relu', padding='same'))
 
-model.add(Conv2D(64, (3, 3), activation='relu', padding='same'))
-model.add(MaxPooling2D(pool_size=(2, 2)))
 
-model.add(Flatten())
-model.add(Dense(128, activation = 'relu'))
-model.add(Dropout(0.5))
-model.add(Dense(1, activation = 'sigmoid'))
-#<----------------Model----------------->
 
-model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+    model.add(MaxPooling2D(pool_size=(2, 2)))
+    model.add(Flatten())
+    model.add(Dense(128, activation = 'relu'))
+    model.add(Dropout(0.5))
+    model.add(Dense(1, activation = 'sigmoid'))
 
+
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+    return model
+
+tuner = RandomSearch(
+    build_model,
+    objective='val_accuracy',
+    max_trials = 1,
+    executions_per_trial=1,
+    directory="./",
+    project_name="tuner_models"
+)
+
+tuner.search(X_train, y_train,epochs=EPOCHS,validation_split=0.1)
+
+best_model = tuner.get_best_models()[0]
+best_mode.save("./best_model")
+
+"""
 model.fit(X_train,y_train, epochs = EPOCHS, batch_size=32,validation_split=0.1)
 
 test_accuracy = model.evaluate(X_test,y_test, batch_size = 32)
 
 print(f'Testing Accuracy: {(test_accuracy[1] * 100)}%')
+"""
