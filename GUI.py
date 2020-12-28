4e104dea166dbf3ee9f0b66943ca1b19645b7535
Lakee Sivaraya <lakeesivaraya@gmail.com>
2020-12-28 16:27:46 +0000

    added a next button to move thorugh images

--- GUI.py
+++ GUI.py
@@ -9,28 +9,58 @@ import matplotlib.pyplot as plt
 from tkinter import *
 from PIL import ImageTk, Image
 
-
+CATEGORIES = ["NORMAL","PNEUMONIA"]
 model = keras.models.load_model("./best_model.h5")
 
 
 path =  "./chest_xray/test/NORMAL/"
 img_path = glob(path + "*.jpeg")
+rand_img = random.sample(img_path,1)[0]
 
-rand_img = random.sample(img_path,1)
-img = cv2.imread(rand_img[0], cv2.IMREAD_GRAYSCALE)
-image_sized = cv2.resize(img,(64,64))
-X = np.array(image_sized).reshape(-1,64,64,1)/255
+def next():
+    category = random.choice(CATEGORIES)
+    path =  "./chest_xray/test/" + category + "/"
+    img_path = glob(path + "*.jpeg")
+    rand_img = random.sample(img_path,1)[0]
+    root.img_temp = ImageTk.PhotoImage((Image.open(rand_img)).resize((300,300)))
+    image_Lab.configure(image=root.img_temp)
+    caption_Lab.configure(text=category)
+
+
+
+
+def model_predict(path):
+    img = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    image_sized = cv2.resize(img,(64,64))
+    X = np.array(image_sized).reshape(-1,64,64,1)/255
+    y = int(round(model.predict(X)[0,0],0))
+    prediction = CATEGORIES[y]
+
+
+
+
+
+
 
 root = Tk()
 root.geometry("300x600")  
-canvas = Canvas(root, width = 300, height = 300)  
-canvas.pack()  
-img_temp = ImageTk.PhotoImage((Image.open(rand_img[0])).resize((300,300)))
+root.title("Chest X-ray prediction")
+root.iconbitmap("./images/icon.ico")
 
 
-canvas.create_image(150, 150, image=img_temp) 
+
+
+root.img_init = ImageTk.PhotoImage((Image.open(rand_img)).resize((300,300)))
+
+image_Lab = Label(root,image = root.img_init)
+image_Lab.pack()
+
+caption_Lab = Label(root, text = "NORMAL")
+caption_Lab.pack()
+
+
+Next_Btn = Button(root,text="Next Image", command = next)
+Next_Btn.pack()
+Next_Btn.place(x=10,y=550)
+
 root.mainloop() 
-
-
-
-print(round(model.predict(X)[0,0],0))
